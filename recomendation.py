fbdd4635d6054cf7275b861d2abbaaddc7ee55ff
Marsvest <alajh.akbar@gmail.com>
2024-10-22 14:16:50 +1000

    update rec

--- recomendation.py
+++ recomendation.py
@@ -1,37 +1,37 @@
 import numpy as np
+from tqdm import tqdm
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import UserPath, EventToCategory
+from models import UserPath
 
 
-class RecommendationSystem:
+class Database:
     def __init__(self, db_connector: str) -> None:
         engine = create_engine(db_connector)
-
         Session = sessionmaker(bind=engine)
         self.session = Session()
 
     def __del__(self) -> None:
         self.session.close()
 
-    def get_all_data(self) -> list[UserPath]:
-        # user_data = self.session.query(UserPath).distinct().all()
-        user_data = self.session.query(UserPath).distinct().limit(100000).all()
-        return user_data
-
-    def get_all_events_id(self) -> list[str]:
-        event_data = self.session.query(EventToCategory).all()
-
-        return [data.EventID for data in event_data]
+    def get_all_data(self) -> tuple[list[UserPath], list[str], list[str]]:
+        all_data = self.session.query(UserPath).limit(100).all()
+        event_data = set(data.EventID for data in all_data)
+        user_data = set(data.UserID for data in all_data)
+        return all_data, list(event_data), list(user_data)
 
 
-recommend_system = RecommendationSystem(db_connector="sqlite:///dump.db")
-events = recommend_system.get_all_events_id()
-table = {}
-for event in events:
-    table[event] = []
+database = Database(db_connector="sqlite:///dump.db")
+all_data, events, users = database.get_all_data()
 
-all_data = recommend_system.get_all_data()
-for data in all_data:
-    table[data.EventID].append(data.UserID)
+matrix = np.zeros((len(events), len(users)), dtype=int)
+
+for data in tqdm(all_data):
+    event_index = events.index(data.EventID)
+    user_index = users.index(data.UserID)
+    matrix[event_index, user_index] = 1
+
+print(matrix)
+
+# TODO: Оптимизировать запросы в бд
